fc8acfb862b51c908c0378b6679bceaaf85c25db
Mathieu Suen <mathk.sue@gmail.com>
2010-08-16 17:32:34 +0200

    Work on return

--- gst-git.c
+++
@@ -1,9 +0,0 @@
-#include <gstpub.h>
-
-// Add the activation records mechanisum
-
-void
-gst_initModule (VMProxy *proxy)
-{
-	
-}
---
+++ gst-jit.c
@@ -0,0 +1,16 @@
+#include <gstpub.h>
+
+// Add the activation records mechanisum
+
+
+void
+gst_initModule (VMProxy *proxy)
+{
+  //_gst_vm_proxy->defineCFunc ("jitUnwindContextAddr", unwind_context_addr);
+}
+
+/* Local Variables: */
+/* mode:c           */
+/* c-basic-offset:2 */
+/* tab-width:2      */
+/* End:             */
